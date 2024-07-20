d2b7d7e9cb074142f0936c296439dd299b5279c9
AdyPutra3234 <romadona.xtl4@gmail.com>
2024-07-20 11:48:08 +0900

    register notes plugin

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const NotesService = require('./services/inMemory/NotesService');
+const notes = require('./api/notes');
 
 const init = async () => {
+  const notesService = new NotesService();
+
   const server = Hapi.server({
     port: 5000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
@@ -13,7 +16,12 @@ const init = async () => {
     },
   });
 
-  server.route(routes);
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
 
   await server.start();
   console.log(`Server running at ${server.info.uri}`);
